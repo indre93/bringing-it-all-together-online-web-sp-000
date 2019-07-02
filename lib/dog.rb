3d17e5fbb3d964e79fa7b68248d12b2036f17391
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-02 05:53:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,10 +2,10 @@ class Dog
   attr_accessor :name, :breed
   attr_reader :id
 
-  def initialize(id = nil, name: ?, breed: ?)
+  def initialize(id = nil, name:, breed:)
     @id = id
-    @name = ?
-    @breed = ?
+    @name = name
+    @breed = breed
   end
 
 end

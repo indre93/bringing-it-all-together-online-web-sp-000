042d245a73c21dfc054a380f82716f7c3704ee95
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-02 06:41:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -49,8 +49,8 @@ class Dog
 
   def self.find_by_id(id)
     sql = "SELECT * FROM dogs WHERE id = ?"
-     DB[:conn].execute(sql, id).first
-    self.new(attr_hash)
+    result = DB[:conn].execute(sql, id).first
+    self.new(result[0], result[1], result[2])
   end
 
 end

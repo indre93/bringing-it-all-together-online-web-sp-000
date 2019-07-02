d4788e18560aeaf12b7115b41601a93e4ac47774
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-02 07:11:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -51,7 +51,7 @@ class Dog
     sql = "SELECT * FROM dogs WHERE id = ?"
     row = DB[:conn].execute(sql, id)[0]
     row.each do |key, value|
-      self.send("#{key}=", value)
+      self.new("#{key}=", value)
     end.first
   end
 

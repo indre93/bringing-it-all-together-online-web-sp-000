7252fcb53810862b20f6c1ff4f5408129264c526
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-02 06:09:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,6 +9,13 @@ class Dog
   end
 
   def self.create_table
+    DB[:conn].execute("DROP TABLE IF EXISTS dogs")
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+        )
 
   end
 

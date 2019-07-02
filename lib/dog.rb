a4d5ff5fd4e107b1a634d21647266c1653a603dc
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-02 07:26:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -70,8 +70,8 @@ class Dog
 
   def self.find_by_name(name)
     sql = "SELECT * FROM dogs WHERE name = ?"
-    row = DB[:conn].execute(sql, name)[0]
-    self.new(name: row[0])
+    row = DB[:conn].execute(sql, name).first
+    self.new(name: row[1])
   end
 
 end

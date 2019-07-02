e6e883560cc1bba57e8b4edbf4632585c68713b5
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-02 07:04:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,7 @@ class Dog
   attr_accessor :id, :name, :breed
 
   def initialize(attr_hash)
-    @id = id
+    @id = nil
     attr_hash.each do |key, value|
       self.send("#{key}=", value)
     end
